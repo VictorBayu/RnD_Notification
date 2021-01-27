a9479f7a5ac744fa6598c83646a41ca1a869cb22
VictorBayu <victorbayu85@gmail.com>
2021-01-28 01:14:50 +0700

    remove old DB

---
+++ DB/rnd_notif.sql
@@ -0,0 +1,228 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 27, 2021 at 07:13 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rnd_notif`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id_contact` int(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `whatsapp` varchar(255) NOT NULL,
+  `telegram` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id_contact`, `email`, `whatsapp`, `telegram`) VALUES
+(1, 'rifaulhilal06@gmail.com', '087856938035', '089522887007'),
+(2, 'victorbayu85@gmail.com', '081230245097', '081230245097'),
+(3, 'achmadhandika0@gmail.com', '087760687562', '082132579727'),
+(5, 'sandypriyo@gmail.com', '085646564565', '085646564565');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id_emp` int(255) NOT NULL,
+  `nip` int(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `id_contact` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id_emp`, `nip`, `name`, `username`, `password`, `id_contact`) VALUES
+(1, 12345, 'Rifaul Hilal S', 'Rifaul06', 'Rifaul06', 1),
+(2, 12346, 'Victor Bayu', 'Victor01', 'Victor01', 2),
+(3, 45678, 'Sandy Priyo', 'Sandi02', 'Sandi02', 5),
+(4, 696960, 'Achmad Handika', 'Handika03', 'Handika03', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formatmessage`
+--
+
+CREATE TABLE `formatmessage` (
+  `id_message` int(11) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `formatmessage`
+--
+
+INSERT INTO `formatmessage` (`id_message`, `message`) VALUES
+(1, 'Dear tim helpdesk,\r\nDibutuhkan penanganan pada nomor tiket TA-102:');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `id_project` int(255) NOT NULL,
+  `project_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`id_project`, `project_name`) VALUES
+(1, 'Kapal Perang'),
+(2, 'Kapal Api'),
+(3, 'Kapal Selam'),
+(4, 'Kapal Ikan'),
+(6, 'Kapal Luar');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_employees`
+--
+
+CREATE TABLE `project_employees` (
+  `id` int(255) NOT NULL,
+  `id_project` int(255) NOT NULL,
+  `id_emp` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project_employees`
+--
+
+INSERT INTO `project_employees` (`id`, `id_project`, `id_emp`) VALUES
+(1, 2, 1),
+(2, 2, 4),
+(3, 4, 2),
+(4, 4, 3),
+(5, 3, 1),
+(7, 3, 3),
+(8, 3, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id_contact`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id_emp`),
+  ADD KEY `id_contact` (`id_contact`);
+
+--
+-- Indexes for table `formatmessage`
+--
+ALTER TABLE `formatmessage`
+  ADD PRIMARY KEY (`id_message`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id_project`);
+
+--
+-- Indexes for table `project_employees`
+--
+ALTER TABLE `project_employees`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_emp` (`id_emp`),
+  ADD KEY `id_project` (`id_project`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id_contact` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id_emp` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `formatmessage`
+--
+ALTER TABLE `formatmessage`
+  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `id_project` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `project_employees`
+--
+ALTER TABLE `project_employees`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`id_contact`) REFERENCES `contact` (`id_contact`);
+
+--
+-- Constraints for table `project_employees`
+--
+ALTER TABLE `project_employees`
+  ADD CONSTRAINT `project_employees_ibfk_1` FOREIGN KEY (`id_emp`) REFERENCES `employee` (`id_emp`),
+  ADD CONSTRAINT `project_employees_ibfk_2` FOREIGN KEY (`id_project`) REFERENCES `project` (`id_project`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
